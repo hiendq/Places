d5d6090ca66982116c0a24746c83d9145c83cd21
Kai <43492495+HIENDQ@users.noreply.github.com>
2020-06-07 22:10:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/product', productRoutes);
 app.get('/', (req, res) => {
     
-    res.status(201).json('OK')
+    res.send('Hello');
 })
 
 app.get('/', (req, res) => {
